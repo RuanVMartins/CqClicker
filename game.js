8c6d0f6287e1db52f43091863c814f1573df38db
RuanVMartins <ruanvitor2019.cedaspy@gmail.com>
2026-03-17 23:30:49 -0300

    Adicionado sistema de pontos e a base do upgrade e melhoria do css

--- game.js
+++ game.js
@@ -1,4 +1,23 @@
-const gameState = {
-    Gatitos: 0,
-    GatitosPorClick: 1,
-};
+
+let coinDisplay = document.querySelector('.coins-cost');
+let parsedCoin = parseFloat(coinDisplay.innerHTML);
+
+let clickerCost = document.querySelector('.clicker-cost');
+let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+
+// função para fazer a imagem do gato mudar e gerar dinheiro ao click
+
+function incrementCoin() {
+    parsedCoin += 1;
+    coinDisplay.innerHTML = parsedCoin;
+}
+
+function buyClicker() {
+    if (parsedCoin >= parsedClickerCost) {
+        parsedCoin -= parsedClickerCost;
+        coinDisplay.innerHTML = parsedCoin;
+    } else {
+        // O alert cria uma janela visível para o jogador
+        alert("Moedas insuficientes para comprar o Clicker!"); 
+    }
+}
